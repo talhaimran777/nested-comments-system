36cae40ac893ff99f01317bf43c88e2ef67f591f
Talha Imran <talhaimran284@gmail.com>
2023-02-19 22:21:54 +0500

    Added createdAt field to fake comments.

--- src/data/comments.js
+++ src/data/comments.js
@@ -5,12 +5,14 @@ const comments = [
     body: `Pretty print actually works with multiple arguments too, where as
       the above expects the second argument to be options to inspect which is
       probably not intentional.`,
+    createdAt: "13.hours.ago",
     replies: [],
   },
   {
     id: 2,
     commentBy: 'Jane Doe',
     body: `Testing out new neovim version.`,
+    createdAt: "10.hours.ago",
     replies: [],
   },
 ];
